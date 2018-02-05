530a35d1dba9c48484294251c20d6014837c94e8
Daniel Ruth <doumstar90@gmail.com>
2018-02-05 23:14:13 +0100

    documentation classe anagramme

--- anagramme.js
+++ anagramme.js
@@ -2,6 +2,7 @@
 
 class GenererAnagramme{
 
+	/* Creer le mot initial avec 7 lettres en utilisants A, B, C, D au hasard */
 	constructor(){
 		this.motInitial = '';
 		for(var i = 0; i<7; i++){
@@ -9,17 +10,21 @@ class GenererAnagramme{
 		}
 	} 
 
+	/* Genere des lettres aleatoires entre A et D */
 	genererLettreAleatoire(){
 		var n = Math.floor(Math.random() * 4);
 		return String.fromCharCode(65 + n); // va convertir les nombres en lettres
 	}
 
+	/* Verifier si le mot m2 peut etre deduis du mot m1 */
 	estValide(m1, m2){
 		var cl1 = this.compterLettres(m1);
 		var cl2 = this.compterLettres(m2);
 		for(var a in cl2){
+			// m2 contient une lettre qui n est pas dans m1
 			if(! (a in cl1)) {
 				return false;
+			// m2 contient une lettre qui est plus present dans m2 que dans m1
 			}else if(cl1[a] < cl2[a]){
 				return false;
 			}
@@ -27,12 +32,13 @@ class GenererAnagramme{
 		return true;
 	}
 
+	/* Verifier si m2 restera valide apres le rajout de lettre a la fin*/
 	rajouterLettreEstValide(mv, lett){
 		var m = mv + lett;
 		return this.estValide(this.motInitial, m) && m.length<=7;
 	}
 
-
+	/* Renvoi une liste des mots valide contruis par le rajout des lettre qui sont dans liste lettre a la fin de m2 */
 	rajouterListeLettreEstValide(mv, listelettre){
 		var res = [];
 		for(var i = 0; i<listelettre.length; i++){
@@ -43,6 +49,7 @@ class GenererAnagramme{
 		return res;
 	}
 
+	/* Creer des mots valide apartir de listeMotValide et listelettre*/
 	rajouterListeLettreListeMotsEstValide(listeMotValide, listelettre){
 		var res = [];
 		for(var i = 0; i<listeMotValide.length; i++){
@@ -54,6 +61,7 @@ class GenererAnagramme{
 		return res;
 	}
 
+	/* Renvoi tout les anagrammes de motInitial */
 	generer(){
 		var res = new Set();
 		res.add("");
@@ -62,6 +70,7 @@ class GenererAnagramme{
 			var liste = new Set(this.rajouterListeLettreListeMotsEstValide(Array.from(res), lettres));
 			res.delete("");
 			liste.delete("");
+			/* Si on ne peut plus trouver des nouveaux mot valide */
 			if(liste.size == res.size){
 				res = liste;
 				break;
